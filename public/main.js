67183f8d2cdbac21de052880aeb3059dc17774e1
Ronald J Nagata Jr <junzo@Ronalds-MBP.lan1>
2019-04-14 21:37:06 -1000

    MVP

--- public/main.js
+++ public/main.js
@@ -1,29 +1,98 @@
 'use strict';
 
-const redditReq = new XMLHttpRequest();
-redditReq.addEventListener('load', test);
-redditReq.open('GET', `https://www.reddit.com/r/javascript.json`);
-redditReq.send();
+let subReddits = [`https://www.reddit.com/r/Unity3D.json`, `https://www.reddit.com/r/blender.json`,
+`https://www.reddit.com/r/javascript.json`, `https://www.reddit.com/r/CivVI.json`,
+`https://www.reddit.com/r/Battletechgame.json`, `https://www.reddit.com/r/Cooking.json`, 
+`https://www.reddit.com/r/space.json`, `https://www.reddit.com/r/Guitar.json`, 
+`https://www.reddit.com/r/FullmetalAlchemist.json`, `https://www.reddit.com/r/Sculpture.json`,  ];
+let listingsArr = [];
+const postsElem =document.querySelector("#posts");
+const favButtonNames = ['#fav-button-one', '#fav-button-two', '#fav-button-three'];
 
-function test(){
-  const redditObj = JSON.parse(this.responseText);
-  const author = document.querySelector("#author");
-  const title = document.querySelector("#title");
-  const time = document.querySelector("#time");
-  const views = document.querySelector("#views");
+favButtonNames.forEach(function(name, index){
+  document.querySelector(name).addEventListener('click', () => requestListings(subReddits[index]));
+});
+document.querySelector('#random-button').addEventListener('click', function(){
+  const num = Math.floor(Math.random() * (subReddits.length - 3) + 3);
+  requestListings(subReddits[num]);
+});
+requestListings(subReddits[0]);
 
-  title.innerHTML = redditObj.data.children[0].data.title;
-  author.innerHTML = `by ${redditObj.data.children[0].data.author}`; 
-  let utcSeconds = redditObj.data.children[0].data.created;
-  console.log(utcSeconds);
-  let d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-  d.setUTCSeconds(utcSeconds);
-  console.log(d);
-  time.innerHTML = d.toUTCString();
-  //views.innerHTML = redditObj returns null for every view_count, as it's disabled. Generate
-  // random number?
+function requestListings(subRedditURL){
+  postsElem.innerHTML = "";
+  const redditReq = new XMLHttpRequest();
+  redditReq.addEventListener('load', loadListings);
+  redditReq.open('GET', subRedditURL);
+  redditReq.send();
+}
 
+function loadListings(){
+  const listingsObj = JSON.parse(this.responseText);
+  listingsArr = listingsObj.data.children;
 
-  
-  
+  listingsArr.forEach(function(data){
+    const sectionNames = ['image', 'title', 'subtitle', 'text-snippet'];
+    const container = document.createElement('div');
+    postsElem.appendChild(container);
+    container.className = 'post';
+    
+    sectionNames.forEach(function(name){
+      const section = document.createElement('div');
+      container.appendChild(section);
+      section.className = name;
+
+      if (name === 'image'){
+        const image = document.createElement('img');
+        image.className = "picture";
+        if (data.data.media_metadata){
+          const imageData = data.data.media_metadata;
+          image.src = `${imageData[Object.keys(imageData)[0]].s.u}`;
+          image.addEventListener('load', (event) => section.appendChild(image));
+        }
+        if (data.data.url !== null){
+          const imageData = data.data.url;
+          image.src = `${imageData}`;
+          image.addEventListener('load', (event) => section.appendChild(image));
+        }
+      }
+
+      if (name === 'title'){
+        let text = document.createElement('h4');
+        section.appendChild(text);
+        text.innerHTML = data.data.title;
+      }
+
+      if (name === 'subtitle'){
+        let text = document.createElement('p');
+        section.appendChild(text);
+        const time = calculateDate(new Date(), data.data.created);
+        text.innerHTML = `by ${data.data.author} \u2022 ${time} \u2022 ${data.data.ups} views`;
+      }
+
+      if (name === 'text-snippet'){
+        let text = document.createElement('p');
+        section.appendChild(text);
+        text.innerHTML = data.data.selftext;
+      }
+    });
+  });
+}
+
+function calculateDate(current, previous){
+  const prevDate = new Date(0);
+  prevDate.setUTCSeconds(previous);
+  if (prevDate.getFullYear() !== current.getFullYear()){
+    // If the post wasn't from this year, then we'd return 'x years ago'
+    return;
+  }
+  if (prevDate.getMonth() !== current.getMonth()){
+    // If the post wasn't from this month, then we'd return 'x months ago'
+    return;
+  }
+  if (prevDate.getDay() !== current.getDay()){
+    // If the post wasn't from today, then we'd return 'x days ago'
+    return;
+  }
+  let diffRaw = current.getTime() - prevDate.getTime();
+  return `${Math.floor((diffRaw / 1000) / 3600)} hours ago`;
 }
